a19c3dcfdab40f340fcdde7ad1813894c670f2e0
Victor Misael <misael1805@gmail.com>
2022-05-14 20:15:33 -0300

    finish implementation

--- tarefa5/src/main/java/tarefa5/GaussLegendre.java
+++ tarefa5/src/main/java/tarefa5/GaussLegendre.java
@@ -4,13 +4,13 @@ import java.util.function.Function;
 
 public final class GaussLegendre {
 
-    final Function<Float, Float> function = aFloat -> (float) (Math.pow(aFloat, 4) + Math.sin(aFloat));
+    private static final Function<Float, Float> function = aFloat -> (float) (Math.pow(aFloat, 2) + Math.sin(aFloat));
 
-    private float pointx(float x1, float xf, float s) {
+    private static float pointx(float x1, float xf, float s) {
         return ((x1 + xf) / 2) + ((xf - x1) / 2);
     }
 
-    public float integrate(float xi, float xf, float roots[], float weights[]) {
+    private static float integrate(float xi, float xf, float roots[], float weights[]) {
 
         float result = 0;
 
@@ -20,21 +20,21 @@ public final class GaussLegendre {
 
     }
 
-    public float twopoints(float xi, float xf) {
+    public static float twopoints(float xi, float xf) {
         final float s = (float) Math.sqrt(1f / 3f);
         float roots[] = {-s, s};
         float weights[] = {1, 1};
         return integrate(xf, xf, roots, weights);
     }
 
-    public float threepoints(float xi, float xf) {
+    public static float threepoints(float xi, float xf) {
         final float s = (float) Math.sqrt(3f / 5f);
         float roots[] = {-s, 0, s};
-        float weights[] = {5 / 9, 8 / 9, 5 / 9};
+        float weights[] = {5f / 9, 8f / 9, 5f / 9};
         return integrate(xi, xf, roots, weights);
     }
 
-    public float fourpoints(float xi, float xf) {
+    public static float fourpoints(float xi, float xf) {
         float roots[] = {-0.861136f, -0.339981f, 0.339981f, 0.861136f};
         float weights[] = {0.347854f, 0.652145f, 0.652145f, 0.347854f};
         return integrate(xi, xf, roots, weights);
--- tarefa5/src/main/java/tarefa5/Tarefa5.java
+++ tarefa5/src/main/java/tarefa5/Tarefa5.java
@@ -1,7 +1,45 @@
 package tarefa5;
 
-public class Tarefa5 {
-    public static void main(String args[]){
+import java.util.function.BiFunction;
 
+public class Tarefa5 {
+    public static void main(String args[]) {
+
+        System.out.println("x^4+cos(x)");
+        System.out.println("Gauss legendre");
+
+        float xi = 2;
+        float xf = 3;
+        System.out.println("2 pontos");
+        iterate(xi, xf, GaussLegendre::twopoints);
+        System.out.println("3 pontos");
+        iterate(xi, xf, GaussLegendre::threepoints);
+        System.out.println("4 pontos");
+        iterate(xi, xf, GaussLegendre::fourpoints);
+    }
+
+
+    private static void iterate(float xi, float xf, BiFunction<Float, Float, Float> function) {
+
+        float error = 100f;
+        float epsilon = 10E-4f;
+        int iterations = 1;
+        float nv = function.apply(xi, xf);
+        System.out.println("Resultado é" + nv);
+        while (error > epsilon) {
+            float ov = nv;
+            iterations = iterations * 2;
+            nv = 0;
+            float delta = (xi - xf) / iterations;
+            int itcount = 0;
+            for (int x = 0; x < iterations; x++) {
+                float initial = xi + x * delta;
+                float ending = initial + delta;
+                nv += ((ending - initial) / 2) * function.apply(xi, xf);
+                itcount++;
+            }
+            error = (Math.abs(ov - nv) / nv);
+            System.out.println("Numero de iterações = " + itcount);
+        }
     }
 }
